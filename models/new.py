63feba9faa2bdb3469eff2b099fd91b5e2d64786
Aadishesh Sharma <43909614+aadishesh16@users.noreply.github.com>
2020-04-07 03:28:10 -0400

    Deleted test file

--- models/new.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-engine = create_engine('sqlite:///data/DisasterResponse.db')
-df =  pd.read_sql_table('messages_table', engine)
-X = df.message.values
-y = df.iloc[:,5:]
